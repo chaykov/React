13e8ff08efc9f7fa824e20f16eeb4f23b8706f45
Chaykov <toumarenshi@gmail.com>
2022-01-31 20:35:34 +0100

    master

---
+++ default_/main.jsx
@@ -0,0 +1,43 @@
+// -------------------------------------------
+
+this.state = {date: new Date()}
+
+
+this.setState({ date: new Date() })
+
+// -------------------------------------------
+
+this.setState((state, props) => ({
+  counter: state.counter + props.increment
+}))
+
+
+this.setState(function(state, props){
+  return {
+    counter: state.counter + props.increment
+  }
+})
+
+// -------------------------------------------
+
+constructor(props){
+  super(props);
+  this.state = {
+    posts: [],
+    comments: []
+  }
+}
+
+componentDidMount(){
+  fetchPosts().then(response => {
+    this.setState({
+      posts: response.posts
+    })
+  })
+
+  fetchComments().then(response => {
+    this.setState({
+      comments: response.comments
+    })
+  })
+}

e959a047280593d53609c0e58facc8fc1d1f144b
Chaykov <toumarenshi@gmail.com>
2022-01-31 20:35:24 +0100

    master

--- my-test/src/index.js
+++ my-test/src/index.js
@@ -20,44 +20,92 @@ import ReactDOM from 'react-dom'
 
 // // setInterval(tick, 1)
 
-function FormattedDate(props){
-    return <h2>Aktualny czas: {props.date.toLocaleTimeString()}.</h2>
+// ----------------------------------------
+
+// function UserGreeting(props) {
+//     return <h1>Witamy ponownie!</h1>;
+// }
+  
+// function GuestGreeting(props) {
+//     return <h1>Proszę się zarejestrować.</h1>;
+// }
+
+// function Greeting(props) {
+//     const isLoggedIn = props.isLoggedIn;
+//     if (isLoggedIn) {
+//       return <UserGreeting />;
+//     }
+//     return <GuestGreeting />;
+// }
+  
+// function LoginButton(props){
+//     return(
+//         <button onClick={props.onClick}>
+//             Zaloguj się
+//         </button>
+//     )
+// }
+
+// function LogoutButton(props){
+//     return (
+//         <button onClick={props.onClick}>
+//             Wyloguj się
+//         </button>
+//     )
+// }
+
+// class LoginControl extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.handleLoginClick = this.handleLoginClick.bind(this);
+//         this.handleLogoutClickk = this.handleLogoutClickk.bind(this);
+//         this.state = {isLoggedIn: false};
+//     }
+
+//     handleLoginClick(){
+//         this.setState({isLoggedIn: true})
+//     }
+
+//     handleLogoutClickk(){
+//         this.setState({isLoggedIn: false})
+//     }
+
+//     render(){
+//         const isLoggedIn = this.state.isLoggedIn;
+//         let button;
+//         if(isLoggedIn){
+//             button = <LogoutButton onClick={this.handleLogoutClickk} />;
+//         }else{
+//             button = <LoginButton onClick={this.handleLoginClick} />;
+//         }
+
+//         return(
+//             <div>
+//                 <Greeting isLoggedIn={isLoggedIn} />
+//                 {button}
+//             </div>
+//         )
+//     }
+// }
+
+function Mailbox(props){
+    const unreadMessages = props.unreadMessages;
+    return (
+        <div>
+            <h1>Cześć!</h1>
+            {unreadMessages.length > 0 &&
+                <h2>
+                    Masz {unreadMessages.length} nieprzeczytanych wiadomości.
+                    
+                </h2>
+            }
+        </div>
+    )
 }
 
-class Clock extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {date: new Date()};
-    }
-
-    componentDidMount(){
-        this.timerID = setInterval(
-            () => this.tick(),
-            100
-        )
-    }
-
-    componentWillUnmount(){
-        clearInterval(this.timerID)
-    }
-
-    tick(){
-        this.setState({
-            date: new Date()
-        })
-    }
-
-    render(){
-        return(
-            <div>
-                <h1>Witaj świecie</h1>
-                <FormattedDate date={this.state.date} />
-            </div>
-        )
-    }
-}
+const messages = ['React', 'Re: React', 'Re:Re: React', 'hehe'];
 
 ReactDOM.render(
-    <Clock />,
+    <Mailbox unreadMessages={messages}/>,
     document.getElementById('root')
 )

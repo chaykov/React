d58f214018c804e8a161c15b9698e2547229a69a
Chaykov <toumarenshi@gmail.com>
2022-02-06 20:15:34 +0100

    master

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,19 +1,20 @@
 import {useState} from 'react'
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
+import Card from './components/shared/Card'
 
 function App(){
     const [feedback, setFeedback] = useState(FeedbackData)
 
-    return (
+    return(
         <>
             <Header />
             <div className="container">
-                <FeedbackList feedback={feedback} />
+                <FeedbackList feedback={feedback}/>
             </div>
         </>
     )
 }
 
-export default App
+export default App;
